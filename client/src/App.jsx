d47ecade85404770777fd8f0e7a7cb02fadee950
Isaac Almanza <isaac.almanza19@gmail.com>
2023-07-04 09:56:11 -0500

    feat: add rainbowkit and ethereum cryptography

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,24 @@ import Wallet from "./Wallet";
 import Transfer from "./Transfer";
 import "./App.scss";
 import { useState } from "react";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
 
   return (
-    <div className="app">
-      <Wallet
-        balance={balance}
-        setBalance={setBalance}
-        address={address}
-        setAddress={setAddress}
-      />
-      <Transfer setBalance={setBalance} address={address} />
-    </div>
+        <div className="app">
+          <Wallet
+            balance={balance}
+            setBalance={setBalance}
+            address={address}
+            setAddress={setAddress}
+          />
+          <Transfer setBalance={setBalance} address={address} />
+          <ConnectButton />
+      </div>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, zora],
+  [
+    //alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'ed406cb9f9e10a7b38493e1405633b6f',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+   <WagmiConfig config={wagmiConfig}>
+    <RainbowKitProvider chains={chains}>
+      <App />
+    </RainbowKitProvider>
+  </WagmiConfig>
   </React.StrictMode>
 );
